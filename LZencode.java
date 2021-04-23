16d60d1eda85e35a8d117748f52623ab266e7948
bw119 <bw119@lab-rg07-08.cms.waikato.ac.nz>
2021-04-23 14:33:08 +1200

    Reads standard input as character

--- LZencode.java
+++ LZencode.java
@@ -1,3 +1,22 @@
+import java.io.*;
+
 public class LZencode {
-    
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start () {
+        //Reader reads 
+        try (Reader reader = new InputStreamReader(System.in)) {
+            int b = reader.read();
+
+            while (b != -1) {
+                System.out.println(b);
+                b = reader.read();
+            }
+            
+        } catch (Exception e){
+            System.out.println("Invalid piped in file");
+        }
+    }
 }
